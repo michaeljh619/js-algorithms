6b4e32fac56fb87aca59a26ec00717a84babac2d
Michael Hart <michaeljh619@yahoo.com>
2019-07-15 17:45:38 -0700

    doc: add jsdocs to OptionalArgument.js

--- src/tools/OptionalArgument.js
+++ src/tools/OptionalArgument.js
@@ -1,3 +1,9 @@
+/**
+ * Used to set an optional parameter with a default value.
+ * @param {any} setValue - The actual parameter you want as optional
+ * @param {any} defaultValue - The value to set the arg to if no value was set in arg
+ * @return {any} Either setValue or defaultValue
+ */
 module.exports = function(setValue, defaultValue){
     if(setValue === undefined){
         return defaultValue;
